369f62648f38870436e3ccdb88900c1b14bcbce1
Bismanazir2 <146629624+Bismanazir2@users.noreply.github.com>
2023-10-08 01:31:28 +0530

    Add files via upload

---
+++ EXP 3(b)write a program to check whether the alphabet is vowel or consonant.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+    char value;
+    printf("enter the character:-");
+    scanf("%c%c",&value,&value);
+    if(value=='a'||value=='e'||value=='i'||value=='o'||value=='u'||value=='A'||value=='E'||value=='I'||value=='O'||value=='U')
+    printf("vowel");
+    else printf("consonant");
+     return 0;
+}
