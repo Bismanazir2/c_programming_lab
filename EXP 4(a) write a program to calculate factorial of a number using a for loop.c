8655183e4b2ec1e2c6a17aee103a52ac152e14cd
Bismanazir2 <146629624+Bismanazir2@users.noreply.github.com>
2023-10-19 00:26:03 +0530

    Add files via upload

---
+++ EXP 4(a) write a program to calculate factorial of a number using a for loop.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+    int n,i;
+    unsigned long long factorial =1;
+    printf("enter an integer:");
+    scanf("%d",&n);
+    if (n<0)
+    
+    printf("Error! factorial is not defined for negative number.\n");
+    
+    else
+    {
+        for (i=1;i<=n;++i)
+        {
+            factorial*=i;
+        }
+      printf("factorial of %d=%11u",n,factorial); 
+    }
+    return 0;
+}
